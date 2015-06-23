3701f10e616ce0eb032a592ad86923a63fdf7792
Kenaniah Cerny <kenaniah@gmail.com>
2015-06-23 07:31:38 -0700

    Using a partial to display feed posts

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,9 +4,16 @@ class SessionController < ApplicationController
   end
 
   def show
+
       redirect_to root_path unless session['auth']
+
       @auth = session['auth']
       @graph = Koala::Facebook::API.new @auth['credentials']['token']
+
+      @stories = @graph.get_object("me/feed?limit=3")
+
+      # render :_feed, layout: false
+
   end
 
   def create
